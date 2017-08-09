b4868c3add0ec464895c1d9ed206f2a47950e639
mrsshell <mrsshell@comcast.net>
2017-08-08 17:54:50 -0700

    This is the complete deal so far

--- app.js
+++ app.js
@@ -1,4 +1,52 @@
 'use strict';
-var whereDo;
-prompt = ('Where do you come from?');
-alert('Welcome to Seattle, I hope you like this as well as' + WhereDo);
+var whereDo = prompt('Where do you come from?');
+alert('I hope you have enjoyed ' + whereDo);
+console.log('Where are you from?');
+
+var dreamJob = prompt('What is your dream job?');
+alert('Your dream job sounds amazing!');
+console.log('Dream job');
+
+var business = prompt('Have you ever thought about starting a business?').toLowerCase();
+if (business === 'yes' || business === 'y') {
+  alert('Good for you!');
+}
+else {
+  alert('Keep dreaming, and one day you will do it!');
+}
+console.log('jobs');
+
+var art = prompt('Do you enjoy fingerpainting?').toLowerCase();
+if (art === 'yes' || art === 'y') {
+  alert('Go ahead, let\'s make a mess!');
+}
+else {
+  alert('Go wash your hands anyway!');
+}
+console.log('Messy art');
+
+var run = prompt('Do you run?').toLowerCase();
+if (run === 'yes' || run === 'y') {
+  alert('YAAY! GO FOR IT!!!');
+}
+else {
+  alert('Take a hike why don\'t you.');
+}
+console.log('Running');
+
+var backpack = prompt('Do you like to backpack?').toLowerCase();
+if (backpack === 'yes' || backpack === 'y') {
+  alert('I bet you\'re a Grearhead too!');
+}
+else {
+  alert('You must be a Trenderfoot.');
+}
+console.log('backpacking');
+
+var twins = prompt('Are my twins identical?').toLowerCase();
+if (twins === 'yes' || twins === 'y') {
+  alert('My twins are NOT identical.');
+}
+else {
+  alert('You are CORRECT, they are boy and girl faternal twins.');
+}
